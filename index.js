7eeefe46f0415982a34096968d1eb0963961020a
teboum <boumazzat@gmail.com>
2022-04-27 23:13:27 +0100

    modeOpératoire

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-const express = require("express")
+const express = require("express");
 const path = require("path");
-const Router = require("./routes/mainRouter")
+const Router = require("./routes/mainRouter");
 
-const app = express()
-
+const app = express();
 
 app.use(express.static(path.join(__dirname, "assets")));
 app.use(express.static(path.join(__dirname, "images")));
 
-
-
 app.set("view engine", "ejs");
 app.set("views", "views"); //2nd folder default
 
-app.use('/',Router)
-
-
-
+app.use("/main", Router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
